f3e58cbf8d882990403183579bf6c9defc99332f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-04-07 08:00:08 +0900

    Update Dijkstra-Shortest-Reach-2.cpp

--- Dijkstra-Shortest-Reach-2.cpp
+++ Dijkstra-Shortest-Reach-2.cpp
@@ -149,20 +149,26 @@ string rtrim(const string &str) {
     return s;
 }
 
-vector<string> split(const string &str) {
+std::vector<string> split(const string &str)
+{
     vector<string> tokens;
+    string_view str_view(str);
+    size_t start = 0;
+    size_t end = 0;
 
-    string::size_type s = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", s)) != string::npos) {
-        tokens.push_back(str.substr(s, end - s));
-
-        s = end + 1;
+    while ((end = str_view.find(' ', start)) != string_view::npos)
+    {
+        if (end > start)
+        {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
     }
 
-    tokens.push_back(str.substr(s));
+    if (start < str_view.size())
+    {
+        tokens.emplace_back(str_view.substr(start));
+    }
 
     return tokens;
 }
-
