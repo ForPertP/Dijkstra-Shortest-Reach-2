82f2145704612e7925d2ba0a45afd4eac8b4d9bf
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-04-08 07:59:45 +0900

    Update Dijkstra-Shortest-Reach-2.cpp

--- Dijkstra-Shortest-Reach-2.cpp
+++ Dijkstra-Shortest-Reach-2.cpp
@@ -138,14 +138,12 @@ string ltrim(const string &str) {
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
-
     s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
     );
-
     return s;
 }
 
