110e76795203107f69e1e74ff0976b739aca38ed
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-04-16 08:27:52 +0900

    Update Dijkstra-Shortest-Reach-2.cs

--- Dijkstra-Shortest-Reach-2.cs
+++ Dijkstra-Shortest-Reach-2.cs
@@ -11,3 +11,18 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+
+class Result
+{
+
+    /*
+     * Complete the 'shortestReach' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY edges
+     *  3. INTEGER s
+     */
+}
