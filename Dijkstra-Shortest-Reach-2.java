dbfbd8a59672514eb31504973723f2217635f496
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-04-22 12:58:46 +0900

    Update Dijkstra-Shortest-Reach-2.java

--- Dijkstra-Shortest-Reach-2.java
+++ Dijkstra-Shortest-Reach-2.java
@@ -1,4 +1,3 @@
-Dijkstra-Shortest-Reach-2
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -10,3 +9,22 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'shortestReach' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY edges
+     *  3. INTEGER s
+     */
+
+    public static List<Integer> shortestReach(int n, List<List<Integer>> edges, int s) {
+    // Write your code here
+
+    }
+
+}
