914a99dc4533c8cc7182b9210d788ae6851824d7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-04-26 09:49:34 +0900

    Update Dijkstra-Shortest-Reach-2.ts

--- Dijkstra-Shortest-Reach-2.ts
+++ Dijkstra-Shortest-Reach-2.ts
@@ -1,1 +1,24 @@
-Dijkstra-Shortest-Reach-2
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let currentLine: number = 0;
+let inputLines: string[] = [];
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split(/\s+/);
+    inputString = ''; 
+    main();
+});
+
+function readInt(): number {
+    return parseInt(inputLines[currentLine++], 10);
+}
