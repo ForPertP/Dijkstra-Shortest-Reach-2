7e2cdc16853845f377d048cfddb4fbaca1ea1ca2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-03 11:08:15 +0900

    Update Dijkstra-Shortest-Reach-2.py

--- Dijkstra-Shortest-Reach-2.py
+++ Dijkstra-Shortest-Reach-2.py
@@ -1,1 +1,12 @@
 Dijkstra-Shortest-Reach-2
+
+#!/bin/python3
+import sys
+import os
+import heapq
+
+input = sys.stdin.readline
+
+def shortestReach(n, adj, s):
+    INF = 10**18
+    dist = [INF] * (n + 1)
