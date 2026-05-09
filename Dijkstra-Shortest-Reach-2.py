f82f81af2ebb8ab462a3700012b276da5a71d212
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-09 16:23:58 +0900

    Update Dijkstra-Shortest-Reach-2.py

--- Dijkstra-Shortest-Reach-2.py
+++ Dijkstra-Shortest-Reach-2.py
@@ -33,3 +33,52 @@ def shortestReach(n, adj, s):
 
     return result
 
+
+#!/bin/python3
+import sys
+import os
+import heapq
+
+input = sys.stdin.readline
+
+
+def shortestReach(n, adj, s):
+    INF = 10**18
+    dist = [INF] * (n + 1)
+
+    dist[s] = 0
+    pq = [(0, s)]
+
+    while pq:
+        d, node = heapq.heappop(pq)
+
+        if d != dist[node]:
+            continue
+
+        for next_node, w in adj[node].items():
+            nd = d + w
+            if dist[next_node] > nd:
+                dist[next_node] = nd
+                heapq.heappush(pq, (nd, next_node))
+
+    result = []
+    for i in range(1, n + 1):
+        if i == s:
+            continue
+        result.append(-1 if dist[i] == INF else dist[i])
+
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for _ in range(t):
+
+
+        fptr.write(' '.join(map(str, result)) + '\n')
+
+    fptr.close()
+
